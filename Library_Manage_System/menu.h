aeb40b3612207412e3cb536c7b7d86b3c6b363cc
Slipstream <1130747753@qq.com>
2023-02-05 18:34:39 +0800

    Changes

---
+++ Library_Manage_System/menu.h
@@ -0,0 +1,12 @@
+//
+// Created by 谷岩 on 2022/12/21.
+//
+
+#ifndef LIBRARY_MANAGE_SYSTEM_MENU_H
+#define LIBRARY_MANAGE_SYSTEM_MENU_H
+int main_menu();          //主菜单
+int catagory_menu();      //分类管理菜单
+void add_book_menu();     //入库菜单
+void delete_book_menu();  //出库菜单
+
+#endif //LIBRARY_MANAGE_SYSTEM_MENU_H
